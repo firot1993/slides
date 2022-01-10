ce8d008d0d26a68e8cfe0778d314893a5c534fce
zping <zping@ebay.com>
2022-01-10 21:48:41 +0800

    Update Kitties.js

--- 03_polkadot-js-api/kitties/frontend/src/Kitties.js
+++ 03_polkadot-js-api/kitties/frontend/src/Kitties.js
@@ -12,31 +12,67 @@ export default function Kitties (props) {
 
   const [kitties, setKitties] = useState([])
   const [status, setStatus] = useState('')
+  const [kittiesOwner, setKittiesOwner] = useState([])
+  const [kittiesDna, setKittiesDna] = useState([])
+
 
   const fetchKitties = () => {
-    // TODO: 在这里调用 `api.query.kittiesModule.*` 函数去取得猫咪的信息。
-    // 你需要取得：
-    //   - 共有多少只猫咪
-    //   - 每只猫咪的主人是谁
-    //   - 每只猫咪的 DNA 是什么，用来组合出它的形态
+    let unsub = null
+
+    const asyncFetch = async () => {
+      unsub = await api.query.kittiesModule.kittiesCount(async cnt => {
+        await api.query.kittiesModule.kitties.entries(async entries => {
+          let kittiesDna = entries.map(([{ args: [era, nominatorId] }, value]) => [era.toHuman(), value]);
+          setKittiesDna(kittiesDna);
+        })
+      })
+    }
+
+    asyncFetch()
+    return () => {
+      unsub && unsub()
+    }
   }
 
   const populateKitties = () => {
-    // TODO: 在这里添加额外的逻辑。你需要组成这样的数组结构：
-    //  ```javascript
-    //  const kitties = [{
-    //    id: 0,
-    //    dna: ...,
-    //    owner: ...
-    //  }, { id: ..., dna: ..., owner: ... }]
-    //  ```
-    // 这个 kitties 会传入 <KittyCards/> 然后对每只猫咪进行处理
     const kitties = []
+    const kittiesIds = []
+    for (let i = 0; i < kittiesDna.length; ++i) {
+      const kitty = {}
+      kitty.id = kittiesDna[i][0];
+      kitty.dna = kittiesDna[i][1].unwrap()
+      for (let j = 0; j < kittiesOwner.length; ++j) {
+        if (kittiesOwner[j][0] == kittiesDna[i][0]){
+          kitty.owner = kittiesOwner[j][1]
+        }
+      }
+      kitties[i] = kitty
+      kittiesIds.push(kitty.id)
+    }
     setKitties(kitties)
+
+    let unsub = null
+    const asyncFetch = async () => {
+        unsub = await api.query.kittiesModule.owner.multi(kittiesIds, async ret => {
+          let owner = []
+          for (let i = 0; i < kittiesIds.length; i++){
+            owner.push([kittiesIds[i], ret[i].toHuman()])
+          }
+          setKittiesOwner(owner)
+        })
+    }
+
+
+    asyncFetch()
+
+    // return the unsubscription cleanup function
+    return () => {
+      unsub && unsub()
+    }
   }
 
   useEffect(fetchKitties, [api, keyring])
-  useEffect(populateKitties, [])
+  useEffect(populateKitties, [keyring,  kittiesDna, kittiesOwner])
 
   return <Grid.Column width={16}>
     <h1>小毛孩</h1>
